go-gitea/gitea

F3: Gitea driver and CLI
user, topic, project, label, milestone, repository, pull_request,
release, asset, comment, reaction, review providers

Signed-off-by: Earl Warren <contact@earl-warren.org>

services/f3/driver/comment.go
@@ -1,0 +1,155 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package driver
+
+import (
+	"fmt"
+
+	"code.gitea.io/gitea/models/db"
+	issues_model "code.gitea.io/gitea/models/issues"
+	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/timeutil"
+	comment_service "code.gitea.io/gitea/services/comments"
+
+	"lab.forgefriends.org/friendlyforgeformat/gof3/forges/common"
+	"lab.forgefriends.org/friendlyforgeformat/gof3/format"
+	"lab.forgefriends.org/friendlyforgeformat/gof3/util"
+)
+
+type Comment struct {
+	issues_model.Comment
+}
+
+func CommentConverter(f *issues_model.Comment) *Comment {
+	return &Comment{
+		Comment: *f,
+	}
+}
+
+func (o Comment) GetID() int64 {
+	return o.Comment.ID
+}
+
+func (o *Comment) SetID(id int64) {
+	o.Comment.ID = id
+}
+
+func (o *Comment) IsNil() bool {
+	return o.ID == 0
+}
+
+func (o *Comment) Equals(other *Comment) bool {
+	return o.Comment.ID == other.Comment.ID
+}
+
+func (o *Comment) ToFormat() *format.Comment {
+	return &format.Comment{
+		Common:      format.Common{Index: o.Comment.ID},
+		IssueIndex:  o.Comment.IssueID,
+		PosterID:    o.Comment.PosterID,
+		PosterName:  o.Comment.Poster.Name,
+		PosterEmail: o.Comment.Poster.Email,
+		Content:     o.Comment.Content,
+		Created:     o.Comment.CreatedUnix.AsTime(),
+		Updated:     o.Comment.UpdatedUnix.AsTime(),
+	}
+}
+
+func (o *Comment) FromFormat(comment *format.Comment) {
+	*o = Comment{
+		Comment: issues_model.Comment{
+			ID:       comment.Index,
+			PosterID: comment.PosterID,
+			Poster: &user_model.User{
+				ID:    comment.PosterID,
+				Name:  comment.PosterName,
+				Email: comment.PosterEmail,
+			},
+			Content:     comment.Content,
+			CreatedUnix: timeutil.TimeStamp(comment.Created.Unix()),
+			UpdatedUnix: timeutil.TimeStamp(comment.Updated.Unix()),
+		},
+	}
+}
+
+type CommentProvider struct {
+	g *Gitea
+}
+
+func (o *CommentProvider) ToFormat(comment *Comment) *format.Comment {
+	return comment.ToFormat()
+}
+
+func (o *CommentProvider) FromFormat(f *format.Comment) *Comment {
+	var comment Comment
+	comment.FromFormat(f)
+	return &comment
+}
+
+func (o *CommentProvider) GetObjects(user *User, project *Project, commentable common.ContainerObjectInterface, page int) []*Comment {
+	comments, err := issues_model.FindComments(o.g.ctx, &issues_model.FindCommentsOptions{
+		ListOptions: db.ListOptions{Page: page, PageSize: o.g.perPage},
+		RepoID:      project.GetID(),
+		IssueID:     commentable.GetID(),
+		Type:        issues_model.CommentTypeComment,
+	})
+	if err != nil {
+		panic(fmt.Errorf("error while listing comment: %v", err))
+	}
+
+	return util.ConvertMap[*issues_model.Comment, *Comment](comments, CommentConverter)
+}
+
+func (o *CommentProvider) ProcessObject(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) {
+	if err := comment.LoadIssue(); err != nil {
+		panic(err)
+	}
+	if err := comment.LoadPoster(); err != nil {
+		panic(err)
+	}
+}
+
+func (o *CommentProvider) Get(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) *Comment {
+	id := comment.GetID()
+	c, err := issues_model.GetCommentByID(o.g.ctx, id)
+	if issues_model.IsErrCommentNotExist(err) {
+		return &Comment{}
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	co := CommentConverter(c)
+	o.ProcessObject(user, project, commentable, co)
+	return co
+}
+
+func (o *CommentProvider) Put(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) *Comment {
+	var issue *issues_model.Issue
+	switch c := commentable.(type) {
+	case *PullRequest:
+		issue = c.PullRequest.Issue
+	case *Issue:
+		issue = &c.Issue
+	default:
+		panic(fmt.Errorf("unexpected type %T", commentable))
+	}
+	c, err := comment_service.CreateIssueComment(o.g.GetDoer(), &project.Repository, issue, comment.Content, nil)
+	if err != nil {
+		panic(err)
+	}
+	return o.Get(user, project, commentable, CommentConverter(c))
+}
+
+func (o *CommentProvider) Delete(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) *Comment {
+	c := o.Get(user, project, commentable, comment)
+	if !c.IsNil() {
+		err := issues_model.DeleteComment(o.g.ctx, &c.Comment)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return c
+}

services/f3/driver/user.go
@@ -1,0 +1,135 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package driver
+
+import (
+	"fmt"
+
+	"code.gitea.io/gitea/models/db"
+	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/util"
+	user_service "code.gitea.io/gitea/services/user"
+
+	"lab.forgefriends.org/friendlyforgeformat/gof3/format"
+	f3_util "lab.forgefriends.org/friendlyforgeformat/gof3/util"
+)
+
+type User struct {
+	user_model.User
+}
+
+func UserConverter(f *user_model.User) *User {
+	return &User{
+		User: *f,
+	}
+}
+
+func (o User) GetID() int64 {
+	return o.ID
+}
+
+func (o *User) SetID(id int64) {
+	o.ID = id
+}
+
+func (o *User) IsNil() bool {
+	return o.ID == 0
+}
+
+func (o *User) Equals(other *User) bool {
+	return (o.Name == other.Name)
+}
+
+func (o *User) ToFormat() *format.User {
+	return &format.User{
+		Common:   format.Common{Index: o.ID},
+		UserName: o.Name,
+		Name:     o.FullName,
+		Email:    o.Email,
+		Password: o.Passwd,
+	}
+}
+
+func (o *User) FromFormat(user *format.User) {
+	*o = User{
+		User: user_model.User{
+			ID:       user.Index,
+			Name:     user.UserName,
+			FullName: user.Name,
+			Email:    user.Email,
+			Passwd:   user.Password,
+		},
+	}
+}
+
+type UserProvider struct {
+	g *Gitea
+}
+
+func (o *UserProvider) ToFormat(user *User) *format.User {
+	return user.ToFormat()
+}
+
+func (o *UserProvider) FromFormat(p *format.User) *User {
+	var user User
+	user.FromFormat(p)
+	return &user
+}
+
+func (o *UserProvider) GetObjects(page int) []*User {
+	users, _, err := user_model.SearchUsers(&user_model.SearchUserOptions{
+		Actor:       o.g.GetDoer(),
+		Type:        user_model.UserTypeIndividual,
+		ListOptions: db.ListOptions{Page: page, PageSize: o.g.perPage},
+	})
+	if err != nil {
+		panic(fmt.Errorf("error while listing users: %v", err))
+	}
+	return f3_util.ConvertMap[*user_model.User, *User](users, UserConverter)
+}
+
+func (o *UserProvider) ProcessObject(user *User) {
+}
+
+func (o *UserProvider) Get(exemplar *User) *User {
+	var user *user_model.User
+	var err error
+	if exemplar.GetID() > 0 {
+		user, err = user_model.GetUserByIDCtx(o.g.ctx, exemplar.GetID())
+	} else if exemplar.Name != "" {
+		user, err = user_model.GetUserByName(o.g.ctx, exemplar.Name)
+	} else {
+		panic("GetID() == 0 and UserName == \"\"")
+	}
+	if user_model.IsErrUserNotExist(err) {
+		return &User{}
+	}
+	if err != nil {
+		panic(fmt.Errorf("user %v %w", exemplar, err))
+	}
+	return UserConverter(user)
+}
+
+func (o *UserProvider) Put(user *User) *User {
+	overwriteDefault := &user_model.CreateUserOverwriteOptions{
+		IsActive: util.OptionalBoolTrue,
+	}
+	u := user.User
+	err := user_model.CreateUser(&u, overwriteDefault)
+	if err != nil {
+		panic(err)
+	}
+	return o.Get(UserConverter(&u))
+}
+
+func (o *UserProvider) Delete(user *User) *User {
+	u := o.Get(user)
+	if !u.IsNil() {
+		if err := user_service.DeleteUser(o.g.ctx, &user.User, true); err != nil {
+			panic(err)
+		}
+	}
+	return u
+}

tests/integration/cmd_f3_test.go
@@ -1,0 +1,111 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package integration
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"io"
+	"net/url"
+	"os"
+	"testing"
+
+	"code.gitea.io/gitea/cmd"
+	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/services/migrations"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/urfave/cli"
+	f3_forges "lab.forgefriends.org/friendlyforgeformat/gof3/forges"
+	f3_util "lab.forgefriends.org/friendlyforgeformat/gof3/util"
+)
+
+func Test_CmdF3(t *testing.T) {
+	onGiteaRun(t, func(*testing.T, *url.URL) {
+		AllowLocalNetworks := setting.Migrations.AllowLocalNetworks
+		setting.Migrations.AllowLocalNetworks = true
+		// without migrations.Init() AllowLocalNetworks = true is not effective and
+		// a http call fails with "...migration can only call allowed HTTP servers..."
+		migrations.Init()
+		AppVer := setting.AppVer
+		// Gitea SDK (go-sdk) need to parse the AppVer from server response, so we must set it to a valid version string.
+		setting.AppVer = "1.16.0"
+		defer func() {
+			setting.Migrations.AllowLocalNetworks = AllowLocalNetworks
+			setting.AppVer = AppVer
+		}()
+
+		//
+		// Step 1: create a fixture
+		//
+		fixture := f3_forges.NewFixture(t, f3_forges.FixtureF3Factory)
+		fixture.NewUser()
+		fixture.NewMilestone()
+		fixture.NewLabel()
+		fixture.NewIssue()
+		fixture.NewTopic()
+		fixture.NewRepository()
+		fixture.NewRelease()
+		fixture.NewAsset()
+		fixture.NewIssueComment()
+		fixture.NewIssueReaction()
+
+		//
+		// Step 2: import the fixture into Gitea
+		//
+		cmd.CmdF3.Action = func(ctx *cli.Context) { cmd.RunF3(context.Background(), ctx) }
+		{
+			realStdout := os.Stdout // Backup Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+
+			set := flag.NewFlagSet("f3", 0)
+			_ = set.Parse([]string{"f3", "--import", "--directory", fixture.ForgeRoot.GetDirectory()})
+			cliContext := cli.NewContext(&cli.App{Writer: os.Stdout}, set, nil)
+			assert.NoError(t, cmd.CmdF3.Run(cliContext))
+			w.Close()
+			var buf bytes.Buffer
+			io.Copy(&buf, r)
+			commandOutput := buf.String()
+			assert.EqualValues(t, "imported\n", commandOutput)
+			os.Stdout = realStdout
+		}
+
+		//
+		// Step 3: export Gitea into F3
+		//
+		directory := t.TempDir()
+		{
+			realStdout := os.Stdout // Backup Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+
+			set := flag.NewFlagSet("f3", 0)
+			_ = set.Parse([]string{"f3", "--export", "--no-pull-request", "--user", fixture.UserFormat.UserName, "--repository", fixture.ProjectFormat.Name, "--directory", directory})
+			cliContext := cli.NewContext(&cli.App{Writer: os.Stdout}, set, nil)
+			assert.NoError(t, cmd.CmdF3.Run(cliContext))
+			w.Close()
+			var buf bytes.Buffer
+			io.Copy(&buf, r)
+			commandOutput := buf.String()
+			assert.EqualValues(t, "exported\n", commandOutput)
+			os.Stdout = realStdout
+
+		}
+
+		//
+		// Step 4: verify the export and import are equivalent
+		//
+		files := f3_util.Command(context.Background(), "find", directory)
+		assert.Contains(t, files, "/label/")
+		assert.Contains(t, files, "/issue/")
+		assert.Contains(t, files, "/milestone/")
+		assert.Contains(t, files, "/topic/")
+		assert.Contains(t, files, "/release/")
+		assert.Contains(t, files, "/asset/")
+		assert.Contains(t, files, "/issue_reaction/")
+	})
+}

redwerkz/forgejo

services/f3/driver/comment.go
@@ -1,0 +1,155 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package driver
+
+import (
+	"fmt"
+
+	"code.gitea.io/gitea/models/db"
+	issues_model "code.gitea.io/gitea/models/issues"
+	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/timeutil"
+	issue_service "code.gitea.io/gitea/services/issue"
+
+	"lab.forgefriends.org/friendlyforgeformat/gof3/forges/common"
+	"lab.forgefriends.org/friendlyforgeformat/gof3/format"
+	"lab.forgefriends.org/friendlyforgeformat/gof3/util"
+)
+
+type Comment struct {
+	issues_model.Comment
+}
+
+func CommentConverter(f *issues_model.Comment) *Comment {
+	return &Comment{
+		Comment: *f,
+	}
+}
+
+func (o Comment) GetID() int64 {
+	return o.Comment.ID
+}
+
+func (o *Comment) SetID(id int64) {
+	o.Comment.ID = id
+}
+
+func (o *Comment) IsNil() bool {
+	return o.ID == 0
+}
+
+func (o *Comment) Equals(other *Comment) bool {
+	return o.Comment.ID == other.Comment.ID
+}
+
+func (o *Comment) ToFormat() *format.Comment {
+	return &format.Comment{
+		Common:     format.Common{Index: o.Comment.ID},
+		IssueIndex: o.Comment.IssueID,
+		PosterID:   o.Comment.PosterID,
+		Content:    o.Comment.Content,
+		Created:    o.Comment.CreatedUnix.AsTime(),
+		Updated:    o.Comment.UpdatedUnix.AsTime(),
+	}
+}
+
+func (o *Comment) FromFormat(comment *format.Comment) {
+	*o = Comment{
+		Comment: issues_model.Comment{
+			ID:       comment.Index,
+			PosterID: comment.PosterID,
+			Poster: &user_model.User{
+				ID: comment.PosterID,
+			},
+			Content:     comment.Content,
+			CreatedUnix: timeutil.TimeStamp(comment.Created.Unix()),
+			UpdatedUnix: timeutil.TimeStamp(comment.Updated.Unix()),
+		},
+	}
+}
+
+type CommentProvider struct {
+	g *Gitea
+}
+
+func (o *CommentProvider) GetReferences(comment *Comment) []common.Reference {
+	return nil
+}
+
+func (o *CommentProvider) ToFormat(comment *Comment) *format.Comment {
+	return comment.ToFormat()
+}
+
+func (o *CommentProvider) FromFormat(f *format.Comment) *Comment {
+	var comment Comment
+	comment.FromFormat(f)
+	return &comment
+}
+
+func (o *CommentProvider) GetObjects(user *User, project *Project, commentable common.ContainerObjectInterface, page int) []*Comment {
+	comments, err := issues_model.FindComments(o.g.ctx, &issues_model.FindCommentsOptions{
+		ListOptions: db.ListOptions{Page: page, PageSize: o.g.perPage},
+		RepoID:      project.GetID(),
+		IssueID:     commentable.GetID(),
+		Type:        issues_model.CommentTypeComment,
+	})
+	if err != nil {
+		panic(fmt.Errorf("error while listing comment: %v", err))
+	}
+
+	return util.ConvertMap[*issues_model.Comment, *Comment](comments, CommentConverter)
+}
+
+func (o *CommentProvider) ProcessObject(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) {
+	if err := comment.LoadIssue(o.g.ctx); err != nil {
+		panic(err)
+	}
+	if err := comment.LoadPoster(o.g.ctx); err != nil {
+		panic(err)
+	}
+}
+
+func (o *CommentProvider) Get(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) *Comment {
+	id := comment.GetID()
+	c, err := issues_model.GetCommentByID(o.g.ctx, id)
+	if issues_model.IsErrCommentNotExist(err) {
+		return &Comment{}
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	co := CommentConverter(c)
+	o.ProcessObject(user, project, commentable, co)
+	return co
+}
+
+func (o *CommentProvider) Put(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) *Comment {
+	var issue *issues_model.Issue
+	switch c := commentable.(type) {
+	case *PullRequest:
+		issue = c.PullRequest.Issue
+	case *Issue:
+		issue = &c.Issue
+	default:
+		panic(fmt.Errorf("unexpected type %T", commentable))
+	}
+	c, err := issue_service.CreateIssueComment(o.g.ctx, o.g.GetDoer(), &project.Repository, issue, comment.Content, nil)
+	if err != nil {
+		panic(err)
+	}
+	return o.Get(user, project, commentable, CommentConverter(c))
+}
+
+func (o *CommentProvider) Delete(user *User, project *Project, commentable common.ContainerObjectInterface, comment *Comment) *Comment {
+	c := o.Get(user, project, commentable, comment)
+	if !c.IsNil() {
+		err := issues_model.DeleteComment(o.g.ctx, &c.Comment)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return c
+}

services/f3/driver/user.go
@@ -1,0 +1,140 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package driver
+
+import (
+	"fmt"
+
+	"code.gitea.io/gitea/models/db"
+	user_model "code.gitea.io/gitea/models/user"
+	"code.gitea.io/gitea/modules/util"
+	user_service "code.gitea.io/gitea/services/user"
+
+	"lab.forgefriends.org/friendlyforgeformat/gof3/forges/common"
+	"lab.forgefriends.org/friendlyforgeformat/gof3/format"
+	f3_util "lab.forgefriends.org/friendlyforgeformat/gof3/util"
+)
+
+type User struct {
+	user_model.User
+}
+
+func UserConverter(f *user_model.User) *User {
+	return &User{
+		User: *f,
+	}
+}
+
+func (o User) GetID() int64 {
+	return o.ID
+}
+
+func (o *User) SetID(id int64) {
+	o.ID = id
+}
+
+func (o *User) IsNil() bool {
+	return o.ID == 0
+}
+
+func (o *User) Equals(other *User) bool {
+	return (o.Name == other.Name)
+}
+
+func (o *User) ToFormat() *format.User {
+	return &format.User{
+		Common:   format.Common{Index: o.ID},
+		UserName: o.Name,
+		Name:     o.FullName,
+		Email:    o.Email,
+		Password: o.Passwd,
+	}
+}
+
+func (o *User) FromFormat(user *format.User) {
+	*o = User{
+		User: user_model.User{
+			ID:       user.Index,
+			Name:     user.UserName,
+			FullName: user.Name,
+			Email:    user.Email,
+			Passwd:   user.Password,
+		},
+	}
+}
+
+type UserProvider struct {
+	g *Gitea
+}
+
+func (o *UserProvider) GetReferences(user *User) []common.Reference {
+	return nil
+}
+
+func (o *UserProvider) ToFormat(user *User) *format.User {
+	return user.ToFormat()
+}
+
+func (o *UserProvider) FromFormat(p *format.User) *User {
+	var user User
+	user.FromFormat(p)
+	return &user
+}
+
+func (o *UserProvider) GetObjects(page int) []*User {
+	users, _, err := user_model.SearchUsers(&user_model.SearchUserOptions{
+		Actor:       o.g.GetDoer(),
+		Type:        user_model.UserTypeIndividual,
+		ListOptions: db.ListOptions{Page: page, PageSize: o.g.perPage},
+	})
+	if err != nil {
+		panic(fmt.Errorf("error while listing users: %v", err))
+	}
+	return f3_util.ConvertMap[*user_model.User, *User](users, UserConverter)
+}
+
+func (o *UserProvider) ProcessObject(user *User) {
+}
+
+func (o *UserProvider) Get(exemplar *User) *User {
+	var user *user_model.User
+	var err error
+	if exemplar.GetID() > 0 {
+		user, err = user_model.GetUserByID(o.g.ctx, exemplar.GetID())
+	} else if exemplar.Name != "" {
+		user, err = user_model.GetUserByName(o.g.ctx, exemplar.Name)
+	} else {
+		panic("GetID() == 0 and UserName == \"\"")
+	}
+	if user_model.IsErrUserNotExist(err) {
+		return &User{}
+	}
+	if err != nil {
+		panic(fmt.Errorf("user %v %w", exemplar, err))
+	}
+	return UserConverter(user)
+}
+
+func (o *UserProvider) Put(user *User) *User {
+	overwriteDefault := &user_model.CreateUserOverwriteOptions{
+		IsActive: util.OptionalBoolTrue,
+	}
+	u := user.User
+	err := user_model.CreateUser(&u, overwriteDefault)
+	if err != nil {
+		panic(err)
+	}
+	return o.Get(UserConverter(&u))
+}
+
+func (o *UserProvider) Delete(user *User) *User {
+	u := o.Get(user)
+	if !u.IsNil() {
+		if err := user_service.DeleteUser(o.g.ctx, &user.User, true); err != nil {
+			panic(err)
+		}
+	}
+	return u
+}

tests/integration/cmd_f3_test.go
@@ -1,0 +1,111 @@
+// Copyright 2022 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package integration
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"io"
+	"net/url"
+	"os"
+	"testing"
+
+	"code.gitea.io/gitea/cmd"
+	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/services/migrations"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/urfave/cli"
+	f3_forges "lab.forgefriends.org/friendlyforgeformat/gof3/forges"
+	f3_util "lab.forgefriends.org/friendlyforgeformat/gof3/util"
+)
+
+func Test_CmdF3(t *testing.T) {
+	onGiteaRun(t, func(*testing.T, *url.URL) {
+		AllowLocalNetworks := setting.Migrations.AllowLocalNetworks
+		setting.Migrations.AllowLocalNetworks = true
+		// without migrations.Init() AllowLocalNetworks = true is not effective and
+		// a http call fails with "...migration can only call allowed HTTP servers..."
+		migrations.Init()
+		AppVer := setting.AppVer
+		// Gitea SDK (go-sdk) need to parse the AppVer from server response, so we must set it to a valid version string.
+		setting.AppVer = "1.16.0"
+		defer func() {
+			setting.Migrations.AllowLocalNetworks = AllowLocalNetworks
+			setting.AppVer = AppVer
+		}()
+
+		//
+		// Step 1: create a fixture
+		//
+		fixture := f3_forges.NewFixture(t, f3_forges.FixtureF3Factory)
+		fixture.NewUser()
+		fixture.NewMilestone()
+		fixture.NewLabel()
+		fixture.NewIssue()
+		fixture.NewTopic()
+		fixture.NewRepository()
+		fixture.NewRelease()
+		fixture.NewAsset()
+		fixture.NewIssueComment()
+		fixture.NewIssueReaction()
+
+		//
+		// Step 2: import the fixture into Gitea
+		//
+		cmd.CmdF3.Action = func(ctx *cli.Context) { cmd.RunF3(context.Background(), ctx) }
+		{
+			realStdout := os.Stdout // Backup Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+
+			set := flag.NewFlagSet("f3", 0)
+			_ = set.Parse([]string{"f3", "--import", "--directory", fixture.ForgeRoot.GetDirectory()})
+			cliContext := cli.NewContext(&cli.App{Writer: os.Stdout}, set, nil)
+			assert.NoError(t, cmd.CmdF3.Run(cliContext))
+			w.Close()
+			var buf bytes.Buffer
+			io.Copy(&buf, r)
+			commandOutput := buf.String()
+			assert.EqualValues(t, "imported\n", commandOutput)
+			os.Stdout = realStdout
+		}
+
+		//
+		// Step 3: export Gitea into F3
+		//
+		directory := t.TempDir()
+		{
+			realStdout := os.Stdout // Backup Stdout
+			r, w, _ := os.Pipe()
+			os.Stdout = w
+
+			set := flag.NewFlagSet("f3", 0)
+			_ = set.Parse([]string{"f3", "--export", "--no-pull-request", "--user", fixture.UserFormat.UserName, "--repository", fixture.ProjectFormat.Name, "--directory", directory})
+			cliContext := cli.NewContext(&cli.App{Writer: os.Stdout}, set, nil)
+			assert.NoError(t, cmd.CmdF3.Run(cliContext))
+			w.Close()
+			var buf bytes.Buffer
+			io.Copy(&buf, r)
+			commandOutput := buf.String()
+			assert.EqualValues(t, "exported\n", commandOutput)
+			os.Stdout = realStdout
+
+		}
+
+		//
+		// Step 4: verify the export and import are equivalent
+		//
+		files := f3_util.Command(context.Background(), "find", directory)
+		assert.Contains(t, files, "/label/")
+		assert.Contains(t, files, "/issue/")
+		assert.Contains(t, files, "/milestone/")
+		assert.Contains(t, files, "/topic/")
+		assert.Contains(t, files, "/release/")
+		assert.Contains(t, files, "/asset/")
+		assert.Contains(t, files, "/reaction/")
+	})
+}